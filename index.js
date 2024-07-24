9c640dba5f4a1f78abdbcaf20c5fb6d1d36ac451
manthan <manthanpkawathe1971@gmail.com>
2024-07-24 05:28:45 +0000

    zala deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('welcome to manthan node  kahiatai app now using for testing');
+ res.send('welcome to manthan node  zala deploy');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
